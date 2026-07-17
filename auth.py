a356c58d0dc8b0974144479396483687362cb700
Manya216 <manyaaa972@gmail.com>
2026-07-18 01:17:23 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,73 +1,102 @@
 from passlib.context import CryptContext
-
 from jose import jwt, JWTError
+from datetime import datetime, timedelta
 
 
-SECRET_KEY = "mysecretkey"
-
-ALGORITHM = "HS256"
-
-
+# Password hashing
 pwd_context = CryptContext(
-
     schemes=["bcrypt"],
-
     deprecated="auto"
 )
 
 
+SECRET_KEY = "your_secret_key_here"
+
+ALGORITHM = "HS256"
+
+
+
+# -----------------------------
 # HASH PASSWORD
+# -----------------------------
+
 def hash_password(password: str):
-    password = password[:72]   # IMPORTANT FIX
+
+    # bcrypt maximum limit
+    password = password[:72]
+
     return pwd_context.hash(password)
 
 
+
+
+# -----------------------------
 # VERIFY PASSWORD
+# -----------------------------
+
 def verify_password(
-
-    plain_password,
-
-    hashed_password
+    plain_password: str,
+    hashed_password: str
 ):
 
+    plain_password = plain_password[:72]
+
     return pwd_context.verify(
-
         plain_password,
-
         hashed_password
     )
 
 
+
+
+# -----------------------------
 # CREATE JWT TOKEN
-def create_access_token(data):
+# -----------------------------
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=15
+    )
+
+
+    to_encode.update(
+        {
+            "exp": expire
+        }
+    )
+
 
     return jwt.encode(
-
-        data,
-
+        to_encode,
         SECRET_KEY,
-
         algorithm=ALGORITHM
     )
 
 
-# VERIFY TOKEN
-def verify_token(token):
+
+
+# -----------------------------
+# VERIFY JWT TOKEN
+# -----------------------------
+
+def verify_token(token: str):
 
     try:
 
         payload = jwt.decode(
-
             token,
-
             SECRET_KEY,
-
             algorithms=[ALGORITHM]
         )
 
+
         return payload
+
 
     except JWTError:
 
         return None
-        
